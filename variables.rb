f41529c383c74023c31bb073221b260f299f8533
robertoahc85 <robertoahc85@gmail.com>
2022-05-26 11:41:38 -0500

    Agregando ejemplo de variable en ruby

---
+++ variables.rb
@@ -0,0 +1,15 @@
+#Sumando dos variables
+x = 10
+y = 20
+suma = x + y
+puts suma
+# multiplicando dos variables
+precio_dolar= 20
+peso_mx= 770
+conversion = precio_dolar * peso_mx
+puts conversion
+#Variables tipo texto
+texto_saludo="Hola desde Mexico"
+puts texto_saludo
+puts "Hasta el proximo codigo"
+
