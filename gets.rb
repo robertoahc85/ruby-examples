dd913eabffa304bbaa80e59a1b459362838a1895
robertoahc85 <robertoahc85@gmail.com>
2022-05-27 13:18:21 -0500

    Agregando el archivo ejemplos get

---
+++ gets.rb
@@ -0,0 +1,11 @@
+print "Cual es tu nombre:"
+#obteniendo texto por gets
+respuesta = gets.chomp
+puts "Hola,#{respuesta}, ya esta listo para la vacaciones?"
+#operaciones con gets
+print  "Dame un numero"
+numero1 = gets.chomp.to_i
+print 'Dame otro numero'
+numero2 = gets.chomp.to_i
+puts suma = numero1 + numero2
+
