b5b2007058d80bac789e1cf38e39239331fd97c1
robertoahc85 <robertoahc85@gmail.com>
2022-06-06 09:44:04 -0500

    Agregando carpeta de desafio rentabilidad

---
+++ desafio_rentablilidad/emprendedor2.rb
@@ -0,0 +1,14 @@
+puts"----------------------------------------------"
+puts "|Bienvenido al sistema de calculo de utilidad|"
+puts"----------------------------------------------"
+# Solicitando informacion al usuario
+print "Ingresa el precio del producto: "
+precio = gets.chomp.to_i
+print "Ingresa el numero de usuario comunes: "
+precio = gets.chomp.to_i
+print "Ingresa el numero de usuario premium: "
+precio = gets.chomp.to_i
+print "Ingresa el numero de usuario gratuitos: "
+precio = gets.chomp.to_i
+print "Ingresa el gasto: "
+precio = gets.chomp.to_i
