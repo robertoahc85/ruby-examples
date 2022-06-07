c72eb345c318eb210fa3037be3d97a108a978034
robertoahc85 <robertoahc85@gmail.com>
2022-06-07 11:49:24 -0500

    Agregando codigo  de iteradores

---
+++ iterator.rb
@@ -0,0 +1,11 @@
+60.times{ |i| puts i*2 }
+puts "-----"
+50.upto(64) do |i|
+    
+puts "Cuando i  vale :#{i} el resultado de la multplicacio es #{i * 3}"
+end
+
+1.upto(5){puts "Hola desde Tlaxcala"}
+5.downto(1){puts "Hola desde Ocotlan"}
+(1..5).each{puts "Ya vengan pronto"}
+
