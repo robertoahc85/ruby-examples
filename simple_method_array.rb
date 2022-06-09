44fad61a466fb5ce4cef164ff780b7930c2039a4
robertoahc85 <robertoahc85@gmail.com>
2022-06-09 09:48:42 -0500

    Agregando metodos array

---
+++ simple_method_array.rb
@@ -0,0 +1,20 @@
+#Declarando arreglos 
+arregloInt = [4923,32,745,3232,89,2,1,898,777,2]
+arregloString = ["Miguel","Paula","Juana","Maria","Natalia","Cesar"]
+#Operaciones con arreglos
+puts arregloInt.size
+puts arregloString.length
+print arregloInt.reverse
+puts ""
+print arregloString.reverse
+puts ""
+print arregloInt.shuffle
+puts ""
+print arregloString.shuffle
+puts ""
+puts arregloInt.include?(7)
+puts arregloString.include?("Cesar")
+arregloString.delete("Miguel")
+arregloString.delete_at(1)
+print arregloString
+
