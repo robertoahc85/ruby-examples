f49c63130cf12afb4506b4c2ec1cc99dbf47e91a
robertoahc85 <robertoahc85@gmail.com>
2022-06-09 10:52:54 -0500

    Agregando ejemplo de push y pop

---
+++ simple_push_pop.rb
@@ -0,0 +1,10 @@
+arreglo1=[1,2,3,4,5]
+arreglo1.push(6)
+print arreglo1
+arreglo1.pop
+print arreglo1
+listaPlatillos=['Enchiladas','Mole','Tostadas','Tlacoyos']
+listaPlatillos.push('Mixiotes')
+print listaPlatillos
+listaPlatillos.pop
+print listaPlatillos
