e40c5ffd221161310a4b9bae6d8e82dd0e29b8a0
robertoahc85 <robertoahc85@gmail.com>
2022-06-10 08:51:21 -0500

    Agrgando ejercicio de segundo a minutos

---
+++ adicto_a_redes.rb
@@ -0,0 +1,8 @@
+minutos = [90,120,500,200,50,30,45,700]
+resultado=['mal','mal','mal','mal','bien','bien','bien','mal']
+
+
+def scan_addicts(minutos)
+
+
+end
---
+++ simple_filter_array.rb
@@ -0,0 +1,13 @@
+numeros = [*1..1000]
+numeros_pares = []
+#  puts numeros.select {|num| num.even? ?  "par" :  "impar"}
+numero.each do |j| 
+    if  j.even?
+ 
+    end      
+end
+print numeros_pares
+
+numeros = [*1..1000]
+numeros_pares = numeros.select { |num| num.even?}
+print numeros_pares
