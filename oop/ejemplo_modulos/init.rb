c43fd4225c57794b68d3ae7fc6766ebf70c45019
robertoahc85 <robertoahc85@gmail.com>
2022-07-18 12:21:34 -0500

    Clase 18 julio

---
+++ oop/ejemplo_modulos/init.rb
@@ -0,0 +1,9 @@
+require_relative 'module/carnivoro.rb'
+require_relative 'module/nadador.rb'
+require_relative 'classes/mamifero.rb'
+require_relative 'classes/gato.rb'
+require_relative 'classes/perro.rb'
+
+jefazo = Perro.new
+puts jefazo.nadar
+
